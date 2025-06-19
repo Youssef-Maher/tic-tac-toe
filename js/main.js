8b8ffa5c989659ad0d4a9bae9179d98a77788a28
Yousef Maher <119488529+Youssef-Maher@users.noreply.github.com>
2025-06-19 13:00:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ reset.addEventListener("click", function () {
   boardArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
   isDraw = false;
   gameIsFinished = false;
-  showResult = false;
   currentTurn = "X";
   result.textContent = "";
+  document.getElementById("instruction").textContent = "X turn";
 });
